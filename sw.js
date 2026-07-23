3d7714d3986bd4316db399f83f995154734809a6
osakasp <miniamikaren@gmail.com>
2026-07-23 19:39:15 -0300

    Service worker v2 (rede primeiro, limpa cache antigo)

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE='copa-grifo-v2';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys()
+      .then(ks=>Promise.all(ks.map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      if(res&&res.ok&&e.request.url.startsWith(self.location.origin)){
+        const clone=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,clone));
+      }
+      return res;
+    }).catch(()=>caches.match(e.request))
+  );
+});
