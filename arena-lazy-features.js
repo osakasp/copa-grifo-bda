5c5958b39f447357badcd57dfb178b36a3f2bc60
osakasp <miniamikaren@gmail.com>
2026-08-05 21:54:40 -0300

    Atualiza cache das imagens de notícias

--- arena-lazy-features.js
+++ arena-lazy-features.js
@@ -38,7 +38,7 @@
     ],
     news: [
       './noticias-bootstrap.js?v=20260727-1',
-      './noticias-bda.js?v=20260805-4',
+      './noticias-bda.js?v=20260805-5',
       './noticias-nav-fix.js?v=20260727-1'
     ],
     tournament: [
