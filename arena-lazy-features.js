81a32f6309698c3e9317eeae9c36f8e06dfeb7dd
osakasp <miniamikaren@gmail.com>
2026-08-06 15:57:07 -0300

    Adiciona ranking histórico de campeões (#42)
    Inclui o ranking consolidado na Sala de Troféus, com pódio, aliases, títulos, pontos e empates.

--- arena-lazy-features.js
+++ arena-lazy-features.js
@@ -4,7 +4,8 @@
   const TABLE_EXPORT_SRC = './exportar-tabela-copa-facil.js?v=20260726-2';
   const BUNDLES = Object.freeze({
     champions: [
-      './champion-banners.js?v=20260805-1'
+      './champion-banners.js?v=20260805-1',
+      './ranking-campeoes.js?v=20260806-1'
     ],
     registrations: [
       './central-inscricoes.js'
