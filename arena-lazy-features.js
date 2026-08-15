ce14aaca5abf23aedb2d7f3e8c26f7ade6771e81
osakasp <miniamikaren@gmail.com>
2026-08-15 16:19:54 -0300

    Corrige acessibilidade no celular (#75)
    Adiciona descrições alternativas às imagens e garante área mínima de toque de 44 × 44 px nos controles móveis.

--- arena-lazy-features.js
+++ arena-lazy-features.js
@@ -8,7 +8,7 @@
     ],
     champions: [
       './champion-banners.js?v=20260808-2',
-      './ranking-campeoes.js?v=20260808-8'
+      './ranking-campeoes.js?v=20260815-1'
     ],
     registrations: [
       './central-inscricoes.js'
