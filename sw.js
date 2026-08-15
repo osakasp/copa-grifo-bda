ccf8ced37bbe470f7a293dd559708bd16cc0cc7b
osakasp <miniamikaren@gmail.com>
2026-08-15 14:36:27 -0300

    Invalida cache para os novos cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v61-active-page-lifecycle';
+const VERSION = 'v62-championship-cards';
 const CACHE_PREFIX = 'arena-bda-';
 const CACHE = Object.freeze({
   shell: `${CACHE_PREFIX}shell-${VERSION}`,
