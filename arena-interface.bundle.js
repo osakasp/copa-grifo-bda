465a4c4d862635fdb5ad344880571aed84885c35
osakasp <miniamikaren@gmail.com>
2026-08-16 13:40:18 -0300

    Remove rolagem horizontal dos campeonatos (#85)
    Limita o transbordamento horizontal e renova os recursos em cache.

--- arena-interface.bundle.js
+++ arena-interface.bundle.js
@@ -1858,7 +1858,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '2026.08.16-copa-facil-championships-v2';
+  const VERSION = '2026.08.16-copa-facil-championships-v3';
   const STYLE_ID = 'arenaCopaFacilInterfaceStyles';
   const THEME_COLOR = '#07100c';
 
@@ -1901,6 +1901,7 @@
       color:var(--cf-text);
       background:var(--cf-bg);
       color-scheme:dark;
+      overflow-x:clip;
     }
 
     body.arena-copa-facil>.app-shell{background:var(--cf-bg)}
