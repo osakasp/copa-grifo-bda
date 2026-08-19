65a5167caa28d71cb188bd611f1537c9a99d4edc
osakasp <miniamikaren@gmail.com>
2026-08-18 21:41:46 -0300

    Corrige modal de login após remoção da Comunidade (#98)
    * Corrige modal de login administrativo após carregar Firebase
    
    * Renova cache para o login administrativo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v82-remove-community';
+const VERSION = 'v83-admin-login-modal-fix';
 const CACHE_PREFIX = 'arena-bda-';
 const CACHE = Object.freeze({
   shell: `${CACHE_PREFIX}shell-${VERSION}`,
