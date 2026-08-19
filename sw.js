430484e766090076bc61de4edfda6fee5b74da64
osakasp <miniamikaren@gmail.com>
2026-08-18 21:36:10 -0300

    Remove Comunidade BDA e mantém acesso administrativo (#97)
    * Remove comunidade da interface e mantém login administrativo
    
    * Renova cache após remover comunidade
    
    * Corrige navegação ao remover comunidade

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v81-login-super-league-sync-fix';
+const VERSION = 'v82-remove-community';
 const CACHE_PREFIX = 'arena-bda-';
 const CACHE = Object.freeze({
   shell: `${CACHE_PREFIX}shell-${VERSION}`,
