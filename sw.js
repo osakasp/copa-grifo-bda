020b56ca360e8a66611c2c2bf35ed4adb611cc5d
osakasp <miniamikaren@gmail.com>
2026-09-19 18:39:31 -0300

    perf: atualizar cache para carregamento rápido

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v136-liga-a-cloud-restore';
-const REV = '20260919-6';
+const VERSION = 'v137-fast-load';
+const REV = '20260919-7';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
