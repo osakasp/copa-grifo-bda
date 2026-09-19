02578aa342bb4059d7ea926030a3a3ca593e1cff
osakasp <miniamikaren@gmail.com>
2026-09-19 19:32:18 -0300

    fix: refresh shell after no-freeze loader change

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v139-stable-fast';
-const REV = '20260919-9';
+const VERSION = 'v140-no-freeze';
+const REV = '20260919-10';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
