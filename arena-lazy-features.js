574655ed470d4ee00be12c5065ad644087fc499f
osakasp <miniamikaren@gmail.com>
2026-09-19 18:06:35 -0300

    chore: atualizar gestor de ligas

--- arena-lazy-features.js
+++ arena-lazy-features.js
@@ -47,7 +47,7 @@
     ],
     tournamentDetail: [
       './confrontos-copa-grifo.js',
-      './gestor-inteligente.js?v=20260919-2',
+      './gestor-inteligente.js?v=20260919-3',
       './classificacao-automatica.js?v=20260822-7',
       './resultados-cards-pro.js?v=20260816-2',
       './placar-mobile-stability.js?v=20260803-1'
