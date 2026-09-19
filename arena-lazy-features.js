1b60af013a92c8076f7b8b7726c667a613e5cd33
osakasp <miniamikaren@gmail.com>
2026-09-19 19:47:32 -0300

    fix: load league table when opening tournament

--- arena-lazy-features.js
+++ arena-lazy-features.js
@@ -43,6 +43,7 @@
     tournament: [
       './cores-automaticas-campeonatos.js?v=20260802-2',
       './liga-participantes-bda.js?v=20260919-2',
+      './pontos-corridos.js?v=20260919-3',
       './arena-liga-fixtures.js?v=20260919-1'
     ],
     tournamentDetail: [
