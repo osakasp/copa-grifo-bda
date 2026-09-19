8725d1a33262dd24d4253ee06e8afdd5c255895c
osakasp <miniamikaren@gmail.com>
2026-09-19 19:57:09 -0300

    fix: refresh league game manager cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v142-leagues-fixed';
-const REV = '20260919-12';
+const VERSION = 'v143-league-games';
+const REV = '20260919-13';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
