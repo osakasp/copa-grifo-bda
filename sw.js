5bb887ceaec7376a84e11e769db96b51498e3f51
osakasp <miniamikaren@gmail.com>
2026-09-19 18:02:37 -0300

    Atualiza cache do sistema de ligas

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v131-liga-fixtures';
-const REV = '20260919-1';
+const VERSION = 'v132-liga-schedule';
+const REV = '20260919-2';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
