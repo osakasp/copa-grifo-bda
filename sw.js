36db128b94faad6a3194d58f27da9abbf888c0ce
osakasp <miniamikaren@gmail.com>
2026-09-19 19:35:15 -0300

    perf: refresh smooth mobile loader

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v140-no-freeze';
-const REV = '20260919-10';
+const VERSION = 'v141-smooth-scroll';
+const REV = '20260919-11';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
