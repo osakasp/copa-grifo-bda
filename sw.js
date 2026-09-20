9dd3d5c3a7b598c1f497cbc1b1aed1a551632633
osakasp <miniamikaren@gmail.com>
2026-09-20 08:20:16 -0300

    perf: reveal the arena before background modules load

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v152-team-badge-sync';
-const REV = '20260920-4';
+const VERSION = 'v153-fast-shell';
+const REV = '20260920-5';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
