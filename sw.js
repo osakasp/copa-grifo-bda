3724b35aaaed12691656258a894bae6d39c71a96
osakasp <miniamikaren@gmail.com>
2026-09-20 09:15:13 -0300

    fix: restore compact mobile navigation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v159-touch-core';
-const REV = '20260920-11';
+const VERSION = 'v160-mobile-nav-fixed';
+const REV = '20260920-12';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
