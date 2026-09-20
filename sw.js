50d88b6af9cdeb93c25cdd1120edead2b064296f
osakasp <miniamikaren@gmail.com>
2026-09-20 08:50:53 -0300

    perf: inline the initial arena shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v156-mobile-lite';
-const REV = '20260920-8';
+const VERSION = 'v157-inline-preview';
+const REV = '20260920-9';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
