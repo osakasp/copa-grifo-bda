4b35f53044c262aa1e2a4d309a6b787410b5fbae
osakasp <miniamikaren@gmail.com>
2026-09-22 07:10:52 -0300

    Refresh cache for admin score edit

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = 'v171-portal-tools';
-const REV = '20260922-01';
+const VERSION = 'v172-admin-score-edit';
+const REV = '20260922-02';
 const AUTH_REV = '20260825-5';
 const AUTH_CONSUMERS_REV = '20260825-4';
 const SITE_HEALTH_REV = '20260825-6';
